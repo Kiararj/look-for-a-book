e34338539e400500cb66a28dc4b4addc6115d1a4
Kiara Johnson <kiarajohnson91@gmail.com>
2023-11-16 13:00:24 -0600

    finished server schemas

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,24 +1,36 @@
-const typeDefs = `
+const typeDefs = gql`
     type User {
-        _id: ID
-        name: String
-        email: String
-        password: String
+        _id: ID!
+        name: String!
+        email: String!
+        bookCount: Int
         savedBooks: [Book] 
     }
+
     type Book {
-        authors: String
+        bookId: ID!
+        authors: [String]
         description: String
-        bookId: String
         image: String
         link: String
         title: String
     }
 
     type Auth {
-        token: ID
+        token: ID!
         user: User
-      }
+    }
+
+    type Query {
+        me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookID: String!, title: String!, authors: [String], description: String, image: String, link: String): User
+        removeBook(bookID: String!): User
+    }
 `;
 
 module.exports = typeDefs;
